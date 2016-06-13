c5bcdd4085ae6f86c82504e9094ce743d983e7d7
miyuki <s13h043@gmail.com>
2016-06-13 10:11:10 +0900

    honban

--- calc.php
+++ calc.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
-<html lang="jp">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>電卓</title>
 </head>
 <body>
-
 <!-- get …… 送信内容がURLとして渡される(初期値) -->
 <!-- post …… 本文(本体)として送信される -->
 <form action="calc.php" method="post">
 
     <!-- 最初の数字 -->
-    <input type="text" name="message1" title="1">
+    <input type="text" name="message1">
 
     <!-- セレクトボタン -->
     <select name="calc" title="2">
@@ -22,63 +21,63 @@
     </select>
 
     <!-- ２つ目の数字 -->
-    <input type="text" name="message2" title="3">
+    <input type="text" name="message2">
 
-    = ?
-
-    <input type="hidden" name="sousin" value="1">
-
+    = ?<br>
 
     <!-- 計算ボタン -->
-    <br><input type="submit" value="送信">
+    <input type="submit" value="送信">
 
     <!-- リセットボタン -->
-    <form><input type="reset" value="リセット"></form>
-
+    <input type="reset" value="リセット">
 
 </form>
 
 <?php
 header("Content-Type: text/html; charset=UTF-8");
 
-// getで数字などを取得
-$message1 = htmlspecialchars($_POST['message1']);
-$message2 = htmlspecialchars($_POST['message2']);
-$calc1 = $_REQUEST['calc'];
 
-// 取得した数字をstring型に変換
-$message3 = (string)$message1;
-$message4 = (string)$message2;
+if(isset($_POST['message1']) && isset($_POST['message2']) && isset($_POST['calc'])) {
+    $message1 = htmlspecialchars($_POST['message1']);
+    $message2 = htmlspecialchars($_POST['message2']);
+    $calc1 = $_POST['calc'];
 
-// 数字だったら計算する、数字じゃなかったらエラーを出力
-if(is_numeric($message1) && is_numeric($message2)){
-    switch($calc1){
-        case "+":
-            $sum = $message3 + $message4;
-            //            print $sum;
-            break;
-        case "-":
-            $sum = $message3 - $message4;
-            //            print $sum;
-            break;
-        case "*":
-            $sum = $message3 * $message4;
-            //            print $sum;
-            break;
-        case "/":
-            $sum = $message3 / $message4;
-            //            print $sum;
-            break;
+    // 取得した数字をstring型に変換
+    $str_message1 = (string)$message1;
+    $str_message2 = (string)$message2;
+
+
+    // 数字だったら計算する、数字じゃなかったらエラーを出力
+    if (is_numeric($str_message1) && is_numeric($str_message2)) {
+        switch($calc1){
+            case "+":
+                $sum = $message1 + $message2;
+                //            print $sum;
+                break;
+            case "-":
+                $sum = $message1 - $message2;
+                //            print $sum;
+                break;
+            case "*":
+                $sum = $message1 * $message2;
+                //            print $sum;
+                break;
+            case "/":
+                if($message1 == 0) {
+                    break;
+                }
+                else{
+                    $sum = $message1 / $message2;
+                    break;
+                }
+        }
+        print ($message1." ".$calc1." ".$message2." = ".$sum."\n");
     }
-    print ($message3." ".$calc1." ".$message4." = ".$sum."\n");
-
-}
-else{
-    if( isset( $_POST['sousin'] ) ){
-        print '半角数字を入力してください';
+    else{
+        print "半角数字を入れてください";
     }
-
 }
+
 ?>
 
 </body>
