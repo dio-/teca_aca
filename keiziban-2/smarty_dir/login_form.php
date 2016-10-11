dc71d7bcd88b7955cdb395b1b4713cd324a5865c
miyuki <s13h043@gmail.com>
2016-10-11 11:13:39 +0900

    honban

--- keiziban-2/smarty_dir/login_form.php
+++ keiziban-2/smarty_dir/login_form.php
@@ -12,17 +12,21 @@ $smarty->cache_dir    = '/home/miyuki/github/teca_aca/keiziban-2/smarty_dir/cach
 $name = $_POST['name'];
 $password = $_POST['password'];
 
+
+
+//print $name;
+//print $password;
+
+$smarty->assign('name', $name);
+$smarty->assign('password', $password);
+
 $smarty->display('login_form.tpl');
-
-
-print $name;
-print $password;
 
 // エラーの初期化
 $errors = array();
 
 if(empty($_POST)) {
-    header("Location: login_form.php");
+    //header("Location: login_.php");
 }else{
     // アカウントの入力判定
     if ($name == '') {
@@ -36,12 +40,14 @@ if(empty($_POST)) {
         $errors['password_length'] = "パスワードは半角英数字で入力して下さい。";
     }
 
+
 }
+
 
 // エラー表示
 if(count($errors) === 0){
-    //print 'ok';
-    $smarty->display( 'login_check.php' );
+    //print 'oklk;
+    $smarty->display( 'login_check.tpl' );
 }else{
     if(count($errors) > 0):
         foreach($errors as $value){
@@ -52,8 +58,9 @@ if(count($errors) === 0){
 }
 
 
+//$smarty->display('login_form.tpl');
 
-header("Location: login_check.php");
+//header("Location: login_check.php");
 
 
 ?>
